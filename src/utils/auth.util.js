5a5a605e06095f82692454b61308c378864a3d9c
BhavnaGurjar <bhavnag340@gmail.com>
2026-02-24 20:22:46 +0530

    remove the storage size limit

--- src/utils/auth.util.js
+++ src/utils/auth.util.js
@@ -101,13 +101,7 @@ const fileFilter = (req, file, cb) => {
 };
 
 
-const upload = multer({
-  storage,
-  fileFilter,
-  limits: {
-    fileSize: 100 * 1024 * 1024, // 100MB
-  },
-});
+const upload = multer({storage});
 
 
 const generateVideoPreview = (inputPath, outputPath, duration = 4, start = 0) => {
